a24af6106d5452f365c0e97422eee22934abb276
daichi777 <a.d.j07071355@gmail.com>
2023-03-17 16:36:11 +0900

    lesson1.13 配列①−①

---
+++ curriculum/src/lesson1_13/Practice1.java
@@ -0,0 +1,19 @@
+package lesson1_13;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		int[] a = {1,2,3,4,5};
+		System.out.print("出力結果：");
+
+		for(int i = 0;i<a.length;i++){
+		
+		System.out.println(a[i]);
+		}
+
+	}
+
+}
+
