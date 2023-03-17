2674292250961318682aca46438ec173eebd115e
daichi777 <a.d.j07071355@gmail.com>
2023-03-17 16:48:50 +0900

    lesson1.32 自己紹介プログラムを作成①

---
+++ curriculum/src/lesson1_32/Person.java
@@ -0,0 +1,30 @@
+package lesson1_32;
+
+class Person{
+public String name;
+public int age;
+public double height;
+public double weight;
+public  static int count = 0;
+
+Person(String name, int age, double height, double weight){
+this.name = name;
+this.age = age;
+this.height = height;
+this.weight = weight;
+this.count++;
+
+}
+
+public double bmi(){
+return this.weight / this.height / this.height;
+}
+
+public void print(){
+System.out.println("名前は" + name + "です");
+System.out.println("年は" + age + "です");
+}
+public static void printCount() {
+    System.out.println("合計" + count + "人です");
+}
+}
